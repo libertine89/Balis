e4453a019fd1558d9288192a3830f61cf3c4325f
Chris <cdraper@mail.co.uk>
2025-09-06 09:09:59 +0100

    ..

--- gui/gui.sh
+++ gui/gui.sh
@@ -5,7 +5,20 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+type model struct{}
+
+func (m model) Init() tea.Cmd { return nil }
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg.(type) {
+	case tea.KeyMsg:
+		return m, tea.Quit
+	}
+	return m, nil
+}
+func (m model) View() string { return "Bubble Tea works!\nPress any key to exit." }
+
 func main() {
-	fmt.Println("Bubble Tea imported successfully!")
-	_ = tea.NewProgram(nil)
+	if err := tea.NewProgram(model{}).Start(); err != nil {
+		fmt.Println("Error:", err)
+	}
 }
