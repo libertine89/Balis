ae5611cc0949d635fce50150e7d40a6017332a59
Chris <cdraper@mail.co.uk>
2025-09-06 09:44:54 +0100

    update to go install

--- gui/gui-install.sh
+++ gui/gui-install.sh
@@ -5,6 +5,7 @@ set -e
 # Go installation
 GO_INSTALL_DIR="/tmp/go"
 GOPATH_DIR="/tmp/go-workspace"
+GOCACHE_DIR="/tmp/go-cache"
 GO_VERSION="1.22.12"
 GO_TARBALL="go${GO_VERSION}.linux-amd64.tar.gz"
 GO_DOWNLOAD_URL="https://go.dev/dl/${GO_TARBALL}"
@@ -13,11 +14,11 @@ GO_DOWNLOAD_URL="https://go.dev/dl/${GO_TARBALL}"
 PROJECT_DIR="/tmp/bubbletea-project"
 GO_FILE_NAME="gui.go"
 MODULE_NAME="bubbletea-test"
+GUI_FILE_URL="https://raw.githubusercontent.com/libertine89/Balis/Refactor/gui/gui.go"
 
 # -------- INSTALL GO --------
-echo "Creating directories..."
-mkdir -p "$GO_INSTALL_DIR"
-mkdir -p "$GOPATH_DIR/bin"
+echo "Creating Go directories..."
+mkdir -p "$GO_INSTALL_DIR" "$GOPATH_DIR/bin" "$GOCACHE_DIR"
 
 echo "Downloading Go $GO_VERSION..."
 curl -L -o "/tmp/${GO_TARBALL}" "$GO_DOWNLOAD_URL"
@@ -25,13 +26,16 @@ curl -L -o "/tmp/${GO_TARBALL}" "$GO_DOWNLOAD_URL"
 echo "Extracting Go to /tmp..."
 tar -C /tmp -xzf "/tmp/${GO_TARBALL}"
 
-echo "Setting environment variables..."
+echo "Setting Go environment variables..."
 export PATH="$GO_INSTALL_DIR/bin:$GOPATH_DIR/bin:$PATH"
 export GOPATH="$GOPATH_DIR"
+export GOCACHE="$GOCACHE_DIR"
+export GOMODCACHE="$GOPATH/pkg/mod"
 
 echo "Go installed successfully!"
 echo "PATH=$PATH"
 echo "GOPATH=$GOPATH"
+echo "GOCACHE=$GOCACHE"
 
 echo "Verify installation:"
 go version
@@ -42,7 +46,7 @@ mkdir -p "$PROJECT_DIR"
 cd "$PROJECT_DIR"
 
 echo "Downloading GUI Go file..."
-curl -LO https://raw.githubusercontent.com/libertine89/Balis/Refactor/gui/gui.go
+curl -LO "$GUI_FILE_URL"
 
 echo "Initializing Go module..."
 go mod init "$MODULE_NAME"
